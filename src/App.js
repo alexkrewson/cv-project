9973d23989c14fbc0d5881824eca21030d72867e
alexkrewson <alex.krewson@gmail.com>
2021-11-29 14:09:50 -0800

    meets requirements

--- src/App.js
+++ src/App.js
@@ -2,36 +2,131 @@ import React, { Component } from 'react';
 import Overview from './components/Overview';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
 
   state = {
-    task: "",
-    tasks: []
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    schoolName: '',
+    schoolDegree: '',
+    schoolStartDate: '',
+    schoolEndate: '',
+    employmentHistory: [],
+    displayState: 'application'
   };
 
-  handleInput = event => {
-    this.setState({ task: event.target.value });
+  handleFirstNameInput = event => {
+    this.setState({ firstName: event.target.value });
+  };
+  handleLastNameInput = event => {
+    this.setState({ lastName: event.target.value });
+  };
+  handleEmailInput = event => {
+    this.setState({ email: event.target.value });
+  };
+  handlePhoneNumberInput = event => {
+    this.setState({ phoneNumber: event.target.value });
+  };
+  handleSchoolNameInput = event => {
+    this.setState({ schoolName: event.target.value });
+  };
+  handleSchoolDegreeInput = event => {
+    this.setState({ schoolDegree: event.target.value });
+  };
+  handleSchoolStartDateInput = event => {
+    this.setState({ schoolStartDate: event.target.value });
+  };
+  handleSchoolEndDateInput = event => {
+    this.setState({ schoolEndDate: event.target.value });
+  };
+  handleEmploymentHistoryInput = event => {
+    this.setState({ employmentHistory: this.state.employmentHistory.concat(event.target.value) });
   };
 
-  onClickBtn = e => {
+  displayPreview = e => {
     e.preventDefault();
-    this.setState({ tasks: this.state.tasks.concat(this.state.task) });
-    // this.setState({ task: '' });
+    this.setState({ displayState: 'preview' });
+  }
+
+  displayEdit = e => {
+    e.preventDefault();
+    this.setState({ displayState: 'edit' });
+    console.log(this.state.firstName)
   }
 
   render() {
-    return (
-      <div>
-        <form onSubmit={this.onClickBtn}>
-          <input onChange={this.handleInput} placeholder="enter new task" />
-          {/* <button onClick={this.onClickBtn}>add to task list</button> */}
-          <button type="submit">Submit</button>
-        </form>
-        <Overview tasks={this.state.tasks} />
-      </div>
-    );
+
+    if (this.state.displayState == 'application') {
+      return (
+        <div>
+          <p>application mode...</p>
+          <form onSubmit={this.displayPreview}>
+            <input onChange={this.handleFirstNameInput} placeholder="First Name:" />
+            <input onChange={this.handleLastNameInput} placeholder="Last Name:" />
+            <input onChange={this.handleEmailInput} placeholder="Emai:" />
+            <input onChange={this.handlePhoneNumberInput} placeholder="Phone Number:" />
+            <input onChange={this.handleSchoolNameInput} placeholder="School Name:" />
+            <input onChange={this.handleSchoolDegreeInput} placeholder="School Degree:" />
+            <input onChange={this.handleSchoolStartDateInput} placeholder="School Start Date:" />
+            <input onChange={this.handleSchoolEndDateInput} placeholder="School End Date:" />
+            <input onChange={this.handleEmploymentHistoryInput} placeholder="Employment History:" />
+
+            <button type="submit">Submit</button>
+          </form>
+          {/* <Overview /> */}
+        </div>
+      );
+    } else if (this.state.displayState == 'preview') {
+      return (
+        <div>
+          <h2>preview mode...</h2>
+          <p>First Name: {this.state.firstName}</p> 
+          <p>Last Name: {this.state.lastName}</p> 
+          <p>Email: {this.state.email}</p> 
+          <p>Phone Number: {this.state.phoneNumber}</p> 
+          <p>School Name: {this.state.schoolName}</p> 
+          <p>School Degree: {this.state.schoolDegree}</p> 
+          <p>School Start Date: {this.state.schoolStartDate}</p> 
+          <p>School End Date: {this.state.schoolEndate}</p> 
+          <p>Employment History: {this.state.employmentHistory}</p> 
+
+          <button onClick={this.displayEdit}>Edit</button>
+          {/* <form onSubmit={this.onClickBtn}>
+            <input onChange={this.handleFirstNameInput} value={this.state.firstName} />
+            <input onChange={this.handleLastNameInput} value={this.state.lastName} />
+            <input onChange={this.handleEmailInput} value={this.state.email} />
+            <input onChange={this.handlePhoneNumberInput} value={this.state.phoneNumber} />
+            <input onChange={this.handleSchoolNameInput} value={this.state.schoolName} />
+            <input onChange={this.handleSchoolDegreeInput} value={this.state.schoolDegree} />
+            <input onChange={this.handleSchoolStartDateInput} value={this.state.schoolStartDate} />
+            <input onChange={this.handleSchoolEndDateInput} value={this.state.schoolEndDate} />
+            <input onChange={this.handleEmploymentHistoryInput} value={this.state.employmentHistory} />
+
+          </form> */}
+        </div>
+      )
+    } else if (this.state.displayState == 'edit') {
+      return (
+        <div>
+          <p>edit mode...</p>
+          <form onSubmit={this.displayPreview}>
+            <input onChange={this.handleFirstNameInput} value={this.state.firstName} />
+            <input onChange={this.handleLastNameInput} value={this.state.lastName} />
+            <input onChange={this.handleEmailInput} value={this.state.email} />
+            <input onChange={this.handlePhoneNumberInput} value={this.state.phoneNumber} />
+            <input onChange={this.handleSchoolNameInput} value={this.state.schoolName} />
+            <input onChange={this.handleSchoolDegreeInput} value={this.state.schoolDegree} />
+            <input onChange={this.handleSchoolStartDateInput} value={this.state.schoolStartDate} />
+            <input onChange={this.handleSchoolEndDateInput} value={this.state.schoolEndDate} />
+            <input onChange={this.handleEmploymentHistoryInput} value={this.state.employmentHistory} />
+
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      )
+    }
+
   }
 }
 
